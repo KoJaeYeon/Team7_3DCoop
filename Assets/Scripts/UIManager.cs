93075a8ced6f223297197a01dfe2210e39fe5339
Ludning <94681711+Ludning@users.noreply.github.com>
2024-05-03 21:05:12 +0900

    UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,33 +5,71 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     
-    public GameObject main;
+    public GameObject setting;
+    public GameObject Sound;
+    public GameObject Music;
+    Slider slider1;
+    Slider slider2;
+    private void Awake()
+    {
+        slider1 =GameObject.Find("Sound").GetComponent<Slider>();
+        slider2 = GameObject.Find("Music").GetComponent<Slider>();
+    }
 
+    //시작버튼
     public void StartButton()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
+    //세팅버튼
     public void SettingButton()
-    { 
-        main.SetActive(true);
+    {
+        setting.SetActive(true);
     }
 
+    //다시시작버튼
     public void ReTurnButton()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
-
+    //메인이동 버튼
     public void MainButton()
     {
        SceneManager.LoadScene("Main");
 
     }
 
+    //돌아가기 버튼
     public void BkButton()
     {
-        main.SetActive(false);
+        setting.SetActive(false);
+    }
+
+    //소리 버튼
+    public void SoundX()
+    { 
+       Sound.SetActive(true);
+        slider1.value = 0;
+    }
+    public void SoundO()
+    {
+        Sound.SetActive(false);
+        slider1.value = 1;
+    }
+
+    //음악버튼
+    public void MusicX()
+    {
+        Music.SetActive(true);
+        slider2.value = 0;
+
+    }
+    public void MusicO()
+    {
+        Music.SetActive(false);
+        slider2.value = 1;
     }
 
 }
