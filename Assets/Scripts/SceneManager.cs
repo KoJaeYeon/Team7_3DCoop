d5ca9122934a055e8f50e2190593afbc050c27e9
Ludning <94681711+Ludning@users.noreply.github.com>
2024-05-03 17:30:35 +0900

    UI

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class SceneManager : Singleton<SceneManager>
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public void Buttonstart()
+    { 
+    
+       
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
