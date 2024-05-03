c1e985b14ebcf133d56fb5960d6bf170c2ab4b16
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-05-03 16:51:43 +0900

    Update PoolManager.cs

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -6,7 +6,8 @@ public class PoolManager : Singleton<PoolManager>
 {
     #region 재연작업공간
     List<GameObject> bulletPools = new List<GameObject>();
-    List<GameObject> EnemyPools = new List<GameObject>();
+    List<GameObject> enemyPools = new List<GameObject>();
+    List<GameObject> itemBoxPools = new List<GameObject>();
 
     #endregion
 
