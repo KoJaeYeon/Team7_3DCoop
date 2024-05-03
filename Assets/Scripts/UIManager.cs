3611bf270fbf6721fdd3fdf840f9b236fd34c9a5
Ludning <94681711+Ludning@users.noreply.github.com>
2024-05-03 18:01:45 +0900

    UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-  
 
+    public GameObject main;
+    public void SettingButton()
+    { 
+        main.SetActive(true);
+
+    }
+
+    public void ReTurnButton()
+    { 
+      
+    }
 
 }
