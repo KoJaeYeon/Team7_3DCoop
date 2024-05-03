d476a6b1f577a70e39c3144da59e7dbc25b8b595
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-05-03 15:48:23 +0900

    tsdf

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     float attackDamage;
+    float attackSpeed;
     // Start is called before the first frame update
     void Start()
     {
