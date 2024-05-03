d003f0e1c5e4877591ee032d988ea738cb1e6eac
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-05-03 18:43:34 +0900

    전략패턴 구현해야함

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponManager : MonoBehaviour
+public class WeaponManager : Singleton<WeaponManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject playerWeapons;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        //weapon 가져오기
+
     }
 }
