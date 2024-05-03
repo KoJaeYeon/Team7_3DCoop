d751f371c48e77b5a2ba212a9f32af697fe2c3b8
KoJaeYeon <79954370+KoJaeYeon@users.noreply.github.com>
2024-05-03 21:27:30 +0900

    Weapon Complete

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class WeaponManager : Singleton<WeaponManager>
 {
-    public GameObject playerWeapons;
+    public MyWeapon[] playerWeapons;
+    public WeaponType weaponType;
 
     private void Awake()
     {
-        //weapon 가져오기
+        //플레이어에게서 weapon 가져오기 또는 할당
+        weaponType = WeaponType.Revolver;
+    }
 
+    public void SetWeapon(WeaponType weaponType)
+    {
+        this.weaponType = weaponType;
+        for(int i = 0; i < playerWeapons.Length; i++)
+        {
+            playerWeapons[i].SetWeapon(weaponType);
+        }
     }
 }
