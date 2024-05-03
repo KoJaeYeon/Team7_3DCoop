206b70584bef52f32cfc4651bc0919c72eaae3d0
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-05-03 17:02:51 +0900

    Update PoolManager.cs

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -24,6 +24,7 @@ public class PoolManager : Singleton<PoolManager>
     private void Awake()
     {
         bulletPools.Clear();
+        enemyPools.Clear();
     }
 
     public GameObject GetEnemy()
