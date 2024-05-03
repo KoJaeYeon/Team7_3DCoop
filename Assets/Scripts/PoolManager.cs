c3cf9cc8c4b6b9978117211760105fc0892e0949
KoJaeYeon <kojy2004@yonsei.ac.kr>
2024-05-03 17:23:36 +0900

    ObjectPooling

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class PoolManager : Singleton<PoolManager>
 {
     #region 재연작업공간
-    List<GameObject> bulletPools = new List<GameObject>();
-    List<GameObject> enemyPools = new List<GameObject>();
+    public GameObject bulletPrefab;
+    public GameObject enemyPrefab;
+    public GameObject itemBoxPrefab;
+
+    public Transform bulletTrans;
+    public Transform enemyTrans;
+    public Transform itemBoxTrans;
+
+    private int itemBoxCount = 30;
+
+    Queue<GameObject> bulletPools = new Queue<GameObject>();
+    Queue<GameObject> enemyPools = new Queue<GameObject>();
     List<GameObject> itemBoxPools = new List<GameObject>();
 
     #endregion
@@ -23,28 +33,59 @@ public class PoolManager : Singleton<PoolManager>
 
     private void Awake()
     {
-        bulletPools.Clear();
-        enemyPools.Clear();
-    }
+        for(int i = 0; i < 500; i++)
+        {
+            GameObject prefab = Instantiate(bulletPrefab, bulletTrans);
+            prefab.SetActive(false);
+            bulletPools.Enqueue(prefab);
+        }
+        for (int i = 0; i < 300; i++)
+        {
+            GameObject prefab = Instantiate(enemyPrefab, enemyTrans);
+            prefab.SetActive(false);
+            enemyPools.Enqueue(prefab);
+        }
 
+        for (int i = 0; i < itemBoxCount; i++)
+        {
+            GameObject prefab = Instantiate(itemBoxPrefab, itemBoxTrans);
+            prefab.SetActive(false);
+            itemBoxPools.Add(prefab);
+        }
+    }
+    public GameObject GetBullet()
+    {
+        GameObject bullet = bulletPools.Dequeue();
+        bulletPools.Enqueue(bullet);
+        bullet.SetActive(true);
+        return bullet;
+    }
     public GameObject GetEnemy()
     {
-        GameObject gameObject = new GameObject();
-        return gameObject;
+        GameObject enemy = enemyPools.Dequeue();
+        enemyPools.Enqueue(enemy);
+        enemy.SetActive(true);
+        return enemy;
     }
 
     public GameObject GetItemBox()
     {
-        GameObject gameObject = new GameObject();
-        return gameObject;
+
+        GameObject itemBox;
+        for (int i = 0; i < itemBoxCount; i++)
+        {
+            if (!itemBoxPools[i].activeSelf)
+            {
+                itemBox = itemBoxPools[i];
+                itemBox.SetActive(false);
+                return itemBox;
+            }
+        }
+        GameObject prefab = Instantiate(itemBoxPrefab, itemBoxTrans);
+        itemBoxPools.Add(prefab);
+        return prefab;
     }
 
-    public GameObject GetBullet()
-    {
-        GameObject gameObject = new GameObject();
-        return gameObject;
-    }
-    
 
 
 }
