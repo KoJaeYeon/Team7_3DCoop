5c61d9471d3e425168b1fc0e36424e2276e46712
junhyungs <163379314+junhyungs@users.noreply.github.com>
2024-05-06 02:49:25 +0900

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -123,7 +123,6 @@ public class Enemy : MonoBehaviour, IHitAble
         EnemyRigidbody.useGravity = false;
         EnemyCollider.enabled = false;
 
-        //���Ͱ� ������ Score ++ �߰�����
         Score();
 
         yield return new WaitForSeconds(1.5f);
