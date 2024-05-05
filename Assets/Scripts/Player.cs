a3fea28b9f44882d002adca20e126159d6c14ea2
noblesi <noblesi1570@gmail.com>
2024-05-05 16:12:41 +0900

    Player숫자 늘리기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject[] playerPrefabs;
+    public int playerCount = 1;
+    public int playerCountMax = 5;
+
     private void Start()
     {
         InitPlayer();
@@ -14,8 +18,49 @@ public class Player : MonoBehaviour
         InitPlayer();
     }
 
-    void InitPlayer()
+    private void InitPlayer()
+    {
+        DeactivePlayer();
+
+        playerPrefabs[0].gameObject.SetActive(true);
+    }
+
+    public void PlayerPlus()
+    {
+        if(playerCount < playerCountMax)
+        {
+            playerCount++;
+            playerPrefabs[playerCount-1].gameObject.SetActive(true);
+            Debug.Log($"{playerCount}, {playerPrefabs[playerCount - 1].name}");
+        }
+
+        else
+        {
+            WeaponManager.Instance.PowerUP();
+        }
+    }
+
+    private void PlayerMinus()
     {
 
+    }
+
+    private void DeactivePlayer()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPlus();
+        }
     }
 }
